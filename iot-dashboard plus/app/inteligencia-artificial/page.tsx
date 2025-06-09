9296c9349d5d815da03fa099eb5250f9a92aa287
FivelRangel <154839566+FivelRangel@users.noreply.github.com>
2025-06-09 03:40:05 -0600

    Add files via upload

---
+++ iot-dashboard plus/app/inteligencia-artificial/page.tsx
@@ -0,0 +1,5 @@
+import { InteligenciaArtificialPage } from "@/components/inteligencia-artificial-page"
+
+export default function InteligenciaArtificial() {
+  return <InteligenciaArtificialPage />
+}
