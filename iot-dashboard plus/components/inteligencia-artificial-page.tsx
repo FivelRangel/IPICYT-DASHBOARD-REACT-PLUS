6dd3788b6837fd64c57d0df77c346a14fc2afd02
FivelRangel <154839566+FivelRangel@users.noreply.github.com>
2025-06-09 13:58:05 -0600

    Update inteligencia-artificial-page.tsx

--- iot-dashboard plus/components/inteligencia-artificial-page.tsx
+++ iot-dashboard plus/components/inteligencia-artificial-page.tsx
@@ -168,7 +168,7 @@ export function InteligenciaArtificialPage() {
         "#": index + 1,
         "Tipo de sensor": dato.tipoSensor,
         "Data preprocesada": dato.dataPreprocesada,
-        "Valor procesado (float)": Number.parseFloat(dato.valorProcesado.replace(",", ".")),
+        "Valor procesado (float)": Number(dato.valorProcesado.toString().replace(",", "."))
         Fecha: new Date(dato.fecha).toLocaleString("es-ES"),
         "Fecha ISO": dato.fecha,
       }))
